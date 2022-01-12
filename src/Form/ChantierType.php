2255e05699e25c01546444b96590e5a38551d666
sabrine123456 <binabenn@outlook.fr>
2022-01-12 16:25:00 +0100

    ajout crud users et chantier

---
+++ src/Form/ChantierType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Chantier;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChantierType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Nom')
+            ->add('Adresse')
+            ->add('Date_debut')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Chantier::class,
+        ]);
+    }
+}
